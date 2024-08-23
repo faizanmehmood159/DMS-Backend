92aa80d2a4d9b027a6c8c611d54ac48c8f4bf5f5
Faizan Mehmood <125080877+faizanmehmood159@users.noreply.github.com>
2024-08-23 16:44:02 +0500

    push

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const AppRoutes = require('./src/routes'); 
+const cors = require('cors');
 const { errorHandler } = require('./src/utills/errorHandler');
 const path = require('path');
 
@@ -11,6 +12,16 @@ const app = express();
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', AppRoutes);
+
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, 
+}));
+
+app.options('*', cors());
 
 connectDB();
 
